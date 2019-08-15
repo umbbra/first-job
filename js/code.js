aa48595e622577a03eded6d1646422ab62557288
Patrycja Paczkowska <umbra@spoko.pl>
2019-08-15 12:02:19 +0200

    add search, edit styles

--- js/code.js
+++ js/code.js
@@ -1,5 +1,34 @@
 const init = () => {
-    console.log("Hello World!");
+
+  const inputSearch = document.querySelector('#search');
+  const allAdvices = document.querySelectorAll("li p.section-paragraph");
+  const toShowSearchSection = document.querySelector(".searchText");
+
+
+  searchText = (e) => {
+    if (e.target.value) {
+      const searchText = e.target.value.toLowerCase();
+      let advices = [...allAdvices];
+
+      
+      advices = advices.filter(adv => adv.textContent.toLowerCase().includes(searchText));
+
+      toShowSearchSection.textContent = "";
+      advices.forEach(adv => toShowSearchSection.appendChild(adv));
+      const main = document.querySelector("main");
+
+      main.textContent = "";
+
+    } else {
+      toShowSearchSection.textContent = "";
+    }
+  }
+
+  inputSearch.value = "";
+  
+  
+  inputSearch.addEventListener('input', searchText);
+ 
 };
 
 document.addEventListener("DOMContentLoaded", init);
